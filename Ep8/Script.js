28fc5338ccae3457a1b303fcd80ab86e7d617677
SaifM15 <saifmulla5450@gmail.com>
2024-09-11 01:19:42 +0530

    complete let,const,var

---
+++ Ep8/Script.js
@@ -0,0 +1,20 @@
+
+
+var a = 10;
+console.log(a)
+
+
+let b = 500;
+
+// let b=544; i  cant declare duplicate because it is let 
+
+console.log(b);
+// in total program we can use anywhere in that specific block
+
+
+const c = 1000;
+console.log(c)
+// const c=555; we cant change value of const because it is constant if we give an empty then it show syntax error and id try with force fully it gives an type error
+
+
+
